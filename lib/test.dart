673ef892c6dd3472c694547366a4de821a4397bd
laxmanPal <laxi27902@gmail.com>
2023-12-23 10:33:34 +0530

    added rupee sign

--- lib/test.dart
+++ lib/test.dart
@@ -8,13 +8,13 @@ class TestPage extends StatelessWidget {
     return const Scaffold(
       body: Column(
         children: [
-          Text("1"),
-          Text("2"),
-          Text("3"),
-          Text("4"),
-          Text("5"),
-          Text("6"),
-          Text("7"),
+          Text("₹ 1"),
+          Text("₹ 2"),
+          Text("₹ 3"),
+          Text("₹ 4"),
+          Text("₹ 5"),
+          Text("₹ 6"),
+          Text("₹ 7"),
         ],
       ),
     );
